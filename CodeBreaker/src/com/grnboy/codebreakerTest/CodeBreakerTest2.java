b874d0ef509167353cc46566be6cacf0ad4956b4
kobayashi <grnboy.hey@gmail.com>
2014-03-17 09:24:33 +0900

    add test case

---
+++ CodeBreaker/src/com/grnboy/codebreakerTest/CodeBreakerTest2.java
@@ -0,0 +1,21 @@
+package com.grnboy.codebreakerTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.grnboy.codebreaker.*;
+
+public class CodeBreakerTest2 {
+	CodeBreaker cb;
+	@Before
+	public void before(){
+		cb = new CodeBreaker();
+	}
+	@Test //テストひな形
+	public void testＭを入力して１０００が返ってくる() {		
+		
+	}
+
+}
