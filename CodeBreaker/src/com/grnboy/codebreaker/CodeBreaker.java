d1307e52fa803beeb8fd2e1ecb4b31fe16089474
kobayashi <grnboy.hey@gmail.com>
2014-03-17 13:40:19 +0900

    GUIにJFrame初めて使った！カオス！とりあえず外枠だけ。

--- CodeBreaker/src/com/grnboy/codebreaker/CodeBreaker.java
+++ CodeBreaker/src/com/grnboy/codebreaker/CodeBreaker.java
@@ -1,11 +1,89 @@
 package com.grnboy.codebreaker;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Random;
 
-public class CodeBreaker {
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.table.DefaultTableModel;
+
+public class CodeBreaker extends JFrame implements ActionListener{
+	private JPanel panel;
+
+    private JPanel centorPanel;
+
+    private JPanel southPanel;
+
+    private JScrollPane sc;
+
+    private DefaultTableModel dtm;
+
+    private JTable resultTable;
+
+    private JComboBox[] inputBox = new JComboBox[3];
+
+    private String[] selector = { "1", "2", "3", "4", "5", "6" };
+
+    private JButton judge;
+
+    public CodeBreaker() {
+        //GUIの初期化
+        //Panelの初期化
+        panel = new JPanel(new BorderLayout());
+        centorPanel = new JPanel();
+        southPanel = new JPanel();
+        //テーブル部。結果表示。今回はJTableを使用。
+        dtm = new DefaultTableModel();
+        dtm.addColumn("1");
+        dtm.addColumn("2");
+        dtm.addColumn("3");
+        dtm.addColumn("HIT");
+        dtm.addColumn("BLOW");
+
+        resultTable = new JTable(dtm);
+        resultTable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+        sc = new JScrollPane(resultTable,
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        centorPanel.add(sc);
+
+        //入力部分の製作
+        for (int i = 0; i < 3; i++) {
+            inputBox[i] = new JComboBox(selector);
+            inputBox[i].setActionCommand("" + i);
+            inputBox[i].addActionListener(this);
+            southPanel.add(inputBox[i]);
+        }
+        judge = new JButton("JUDGE!!");
+        judge.setActionCommand("JUDGE");
+        judge.addActionListener(this);
+        southPanel.add(judge);
+
+        //テーブル部、入力部のセット。
+        panel.add(centorPanel, BorderLayout.CENTER);
+        panel.add(southPanel, BorderLayout.SOUTH);
+        this.getContentPane().add(panel);
+
+        //表示
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setBounds(100, 100, 400, 500);
+        this.pack();
+        this.setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent arg0) {
+    }
+        
 	public static void main(String[] args) {
 		//作成したクラスの初期化
 		CBEngine cbe = new CBEngine();
@@ -62,15 +140,8 @@ public class CodeBreaker {
 				}
 			}
 			
-			
-//			for (int i = 0; i <= 2; i++) {
-//				System.out.println(inputNum[i]);
-//			}
-			
-			// 答え判断			
-			
 			// 終了判断　ヒットが3つになったら終了
-			if (cbe.judgeAnswer()) {
+			if (cbe.judgeAnswer()) { //ここでカードのマッチ判定のメソッドを実行してる
 				System.out.println("３つのカード番号を当てました！おめでとうございます！");
 				System.out.println(countChallenge + "回目のチャレンジでした。");
 				break;
